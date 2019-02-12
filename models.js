fada02447436f854ccdb196cd882cbc16862960e
jmutt78 <jmcintosh@docusites.com>
2019-02-12 15:03:47 -0800

    -

--- models.js
+++ models.js
@@ -3,25 +3,17 @@
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
-
-
-
-
-
-
-
-
 const ideaSchema = mongoose.Schema({
   idea: { type: String },
   description: { description: String },
-  questions: [
+  questions: {
     question1: { type: String },
     question2: { type: String },
     question3: { type: String },
     question4: { type: String },
     question5: { type: String },
     question6: { type: String }
-  ]
+  }
 });
 
 ideaSchema.methods.serialize = function() {
@@ -29,14 +21,14 @@ ideaSchema.methods.serialize = function() {
     id: this._id,
     idea: this.idea,
     description: this.description,
-    questions: [
+    questions: {
       question1: this.question1,
       question2: this.question2,
       question3: this.question3,
       question4: this.question4,
       question5: this.question5,
       question6: this.question6
-    ]
+    }
   };
 };
 
